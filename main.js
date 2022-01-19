bf519ab8d3f62488a85711ff3c2726e8aa36d5c9
Zyy <50652852+Azyansah@users.noreply.github.com>
2022-01-19 20:33:27 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-const { exec,execSync,spawn } = require('child_process');
-const { WAConnection, Functions } = require('./lib/functions.js');
-const Command = require('./lib/command.js');
+const { exec, execSync, spawn } = require("child_process");
+const { WAConnection, Functions } = require("./lib/functions.js");
+const Command = require("./lib/command.js");
 
-global.baileys = require('@adiwajshing/baileys');
-global.botinfo = require('./src/json/botInfo.json');
-global.userDb = require('./src/json/user.json');
-global.groupDb = require('./src/json/group.json');
+global.baileys = require("@adiwajshing/baileys");
+global.botinfo = require("./src/json/botInfo.json");
+global.userDb = require("./src/json/user.json");
+global.groupDb = require("./src/json/group.json");
 global.functions = new Functions();
 global.client = new WAConnection();
 global.cmd = new Command(client, botinfo, functions);
-global.logo = {buffer:functions.fs.readFileSync("./src/images/20210929_225016.png")}
+global.logo = {
+  buffer: functions.fs.readFileSync("./src/images/20210929_225016.png"),
+};
 global.clients = [];
-global.used_logo = 0
+global.used_logo = 0;
 
 try {
-functions.run(client)
-} catch (e){
-console.log(e)
+  functions.run(client);
+} catch (e) {
+  console.log(e);
+  client.sendMessage(
+    botinfo.ownerNumber[0] + `@s.whatsapp.net`,
+    "Info Error!",
+    "conversation"
+  );
+  client.sendMessage(
+    botinfo.ownerNumber[0] + `@s.whatsapp.net`,
+    functions.util.format(e),
+    "conversation"
+  );
+  return 1;
 }
